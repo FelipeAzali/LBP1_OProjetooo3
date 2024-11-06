1c2cdcb7a3405534727b6fda272fab88706a4c85
FelipeAzali <129297409+FelipeAzali@users.noreply.github.com>
2024-11-05 21:52:12 -0300

    Delete Modelo/main.py

--- Modelo/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-from controller.blueprint import pagina
-
-app = Flask(__name__)
-app.register_blueprint(pagina)
-
-if __name__ == "__main__":
-   app.run(debug=True)
