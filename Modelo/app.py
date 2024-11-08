c612a440e379c1d8028e8555d17849551de73108
FelipeAzali <129297409+FelipeAzali@users.noreply.github.com>
2024-11-08 20:47:39 -0300

    Update app.py

--- Modelo/app.py
+++ Modelo/app.py
@@ -2,44 +2,44 @@ from flask import Flask, render_template, session, request, redirect, url_for
 from controllers import login, adm, carrinho
 from models import session, items
 
-aplicativo = Flask(__name__)
-aplicativo.register_blueprint(login.c)
-aplicativo.register_blueprint(adm.c)
-aplicativo.register_blueprint(carrinho.c)
-aplicativo.secret_key = 'senha super secreta'
+app = Flask(__name__)
+app.register_blueprint(login.c)
+app.register_blueprint(adm.c)
+app.register_blueprint(carrinho.c)
+app.secret_key = 'senha super secreta'
 
-@aplicativo.errorhandler(404)
+@app.errorhandler(404)
 def pagina_nao_encontrada(error):
     return 'Página não encontrada', 404
 
-@aplicativo.errorhandler(401)
+@app.errorhandler(401)
 def acesso_proibido(error):
     return 'Acesso negado!', 401
 
-@aplicativo.before_requisicao
+@app.before_requisicao
 def b4_requisicao():
-    if requisicao.path == '/login':
+    if request.path == '/login':
         return
     if not session.existe('nome'):
-        return redirecionar(url_para('login.login'))
+        return redirect(url_for('login.login'))
 
-@aplicativo.after_requisicao
+@app.after_requisicao
 def a_requisicao(response):
     print("Depois da requisição")
     return response
 
-@aplicativo.route('/')
+@app.route('/')
 def index():
     carr = []
     for p in items.LISTA_PRODUTOS:
-        cookie = requisicao.cookies.get(f'produto_{p.id}')
+        cookie = request.cookies.get(f'produto_{p.id}')
         if cookie:
             carr.append({
                 'nome': p.nome,
                 'qntd': cookie,
                 'total': int(cookie)*p.preco
             })
-    return renderizar_template('index.html', nome=session.get('nome'), produtos = items.LISTA_PRODUTOS, carrinho=carr)
+    return render_template('index.html', nome=session.get('nome'), produtos = items.LISTA_PRODUTOS, carrinho=carr)
 
 if __name__ == '__main__':
-    aplicativo.run(debug=True)
+    app.run(debug=True)
